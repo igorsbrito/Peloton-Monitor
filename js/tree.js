862ea8ecd58a34c7f2917eadc2564936436daeee
igorsbrito <igorsbrito93@gmail.com>
2017-06-20 11:02:45 -0300

    first commit

---
+++ js/tree.js
@@ -0,0 +1,60 @@
+var tree= [];
+
+
+function getTree() {
+  // Some logic to retrieve, or generate tree structure
+  tree = [{
+    text: "Connections",
+    nodes:[
+      {
+      text:"banco1",
+        nodes:[
+          {
+          text:"tabela 1",
+          },
+          {
+            text:"tabela 2"
+          }
+        ]
+      },
+      {
+      text:"banco1",
+        nodes:[
+          {
+          text:"tabela 1"
+          },
+          {
+            text:"tabela 2"
+          }
+        ]
+      },
+    ]
+    }];
+    console.log(tree);
+    return tree;
+}
+
+$('#tree').treeview({data: getTree(),levels: 2,  showBorder: false });
+
+
+//databaseObject = {databaseName: <name_database>, tables:[<name_table_1>,<name_table_2>,<name_table_3>]}
+function addDatabase(databaseObject){
+	var tables =[];
+	databaseObject.tables.forEach(function(table){
+		tables.push(addTable(table));
+	});
+	
+
+	var database = {text: "databaseName",nodes:tables}
+
+	tree[0].nodes.push(database);
+
+	$('#tree').treeview({data: tree,levels: 2,  showBorder: false });
+}
+
+function addTable(tableName){
+	var table = {text: tableName};
+	return table
+}
+
+// addDatabase({"databaseName":"testeBanco", "tables":["tabelaTeste1","tabelaTeste2"]})
